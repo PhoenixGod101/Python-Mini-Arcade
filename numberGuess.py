1a78f317a938ff4a446c8456ae63411b5ccd7506
PhoenixGod101 <122747571+PhoenixGod101@users.noreply.github.com>
2025-05-19 20:04:16 +0100

    Started Development

--- numberGuess.py
+++ numberGuess.py
@@ -1,1 +1,15 @@
  # Text Based Number guess game
+import random
+
+# Setting variables
+print("Please choose the minimum and maximum value for the random number.")
+RandomMin = int(input("Minimum: "))
+RandomMax = int(input("Maximum: "))
+RandomVal = random.randint(RandomMin, RandomMax)
+
+Guess = 0
+Count = 0
+
+# Game
+print("The random number has been selected. Make your guess!")
+Guess = int(input("Guess: "))
