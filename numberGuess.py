303b6ec9f9b93f316a863e8c9134a613b0f996e5
PhoenixGod101 <122747571+PhoenixGod101@users.noreply.github.com>
2025-05-20 16:47:48 +0100

    Started Development

--- numberGuess.py
+++ numberGuess.py
@@ -1,15 +1,29 @@
  # Text Based Number guess game
 import random
+import sys
 
 # Setting variables
 print("Please choose the minimum and maximum value for the random number.")
-RandomMin = int(input("Minimum: "))
-RandomMax = int(input("Maximum: "))
-RandomVal = random.randint(RandomMin, RandomMax)
+randomMin = int(input("Minimum: "))
+randomMax = int(input("Maximum: "))
+randomVal = random.randint(randomMin, randomMax)
 
-Guess = 0
-Count = 0
+guess = 0
+count = 0
+end = False
+
+# Functions
+def check():
+    if guess == randomVal:
+        count += 1
+        end == True
+        print("Well done! You succesfully guessed the correct number! The number was {}".format(randomVal))
+        if count >= 2:
+            print("It took you {} attempts to guess the correct number!".format(count))
+        else:
+            print("It took you {} attempt to guess the correct number!".format(count))
+        sys.exit()
 
 # Game
 print("The random number has been selected. Make your guess!")
-Guess = int(input("Guess: "))
+guess = int(input("Guess: "))
